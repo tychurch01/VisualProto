02d040236ebe27fb90a3b9d185f5bc9ae8c4e7dd
Ty Church <ty.church@student.adelaide.edu.au>
2024-12-03 18:46:02 +1100

    removed obvious comments

--- Main.py
+++ Main.py
@@ -5,12 +5,12 @@ def main():
     visualizer = VisualArray(rows=5, cols=5, cell_size=0.15, border_width=0.01)
 
 
-    visualizer.update_cell(0, 0, (1, 0, 0))  # Red
-    visualizer.update_cell(1, 1, (0, 1, 0))  # Green
-    visualizer.update_cell(2, 2, (0, 0, 1))  # Blue
+    visualizer.update_cell(0, 0, (1, 0, 0))
+    visualizer.update_cell(1, 1, (0, 1, 0))
+    visualizer.update_cell(2, 2, (0, 0, 1))
 
-    visualizer.update_cell(2, 3, (1, 1, 0))  # Update a cell to yellow
-    visualizer.update_cell(4, 4, (0, 1, 1))  # Update another cell to cyan
+    visualizer.update_cell(2, 3, (1, 1, 0))
+    visualizer.update_cell(4, 4, (0, 1, 1))  
 
     visualizer.run()  # The run method contains the event loop and the rendering process
 
